95b27815f3fdb47ec6975505e15cfda773e707b4
AlemolAMG <alemolamg@outlook.com>
2022-05-14 10:03:26 +0200

    ADD archivo lector de botón externo

--- conteo.py
+++ conteo.py
@@ -4,9 +4,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN)
 count = 0
 while True:
-	inputValue = GPIO.input(24)
-	if (inputValue == True):
-		count = count + 1
-		print("Button pressed " + str(count) + " times.")
-		time.sleep(0.3)
-	time.sleep(.01)
+    inputValue = GPIO.input(24)
+    if (inputValue == True):  # Cambiar el contenido del if por
+        count = count + 1
+        print("Button pressed " + str(count) + " times.")
+        time.sleep(0.3)
+    time.sleep(.01)
