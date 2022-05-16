d2808d96a9dfa3e0761b68c0395c83bbbfb6814e
AlemolAMG <alemolamg@outlook.com>
2022-05-17 01:12:21 +0200

    UP CONTEO2

--- conteo2.py
+++ conteo2.py
@@ -1,14 +1,11 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
-#GPIO.setup(24, GPIO.IN)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-count = 0
+
 while True:
-    inputValue = GPIO.input(24)
-    if (inputValue == False):  # Cambiar el contenido del if por
-        #count = count + 1
-        #print("Button pressed " + str(count) + " times.")
+    Status = GPIO.input(24)
+    if Status == False:
         print("Botón pulsado")
-        # time.sleep(0.3)
-    time.sleep(.01)
+        time.sleep(0.2)
