a660235cbab71206084ce4e3c5cf56f2a38e722c
AlemolAMG <alemolamg@outlook.com>
2022-05-17 00:44:42 +0200

    Cambio ruta archivo audio

--- alternativaSorpresa.py
+++ alternativaSorpresa.py
@@ -2,6 +2,6 @@ import os
 
 # comienza el script
 rutaArchivo = "./audio/sorpresa.mp3"    # ruta del archivo de audio
-os.system('mpg123 {rutaArchivo}')
+os.system('mpg123 ./audio/sorpresa.mp3')
 # playsound(rutaArchivo)  # reproduce audio del archivo que encuentre.
 print("Fin del programa")
